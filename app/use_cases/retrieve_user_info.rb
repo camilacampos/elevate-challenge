66954a787b0792834f8d1b2c24fa5d9a86c3c2ce
Camila Campos <camilacamposramos.si@gmail.com>
2025-06-16 16:42:15 -0300

    add subscription status to user#show api
    Signed-off-by: Camila Campos <camilacamposramos.si@gmail.com>

--- app/use_cases/retrieve_user_info.rb
+++ app/use_cases/retrieve_user_info.rb
@@ -1,11 +1,28 @@
 class RetrieveUserInfo
+  def initialize(billing_api: Billing::Api.new)
+    @billing_api = billing_api
+  end
+
   def call(user)
+    status, err = retrieve_subscription_status(user)
+    return [nil, err] if err
+
     info = {
       id: user.id,
       email: user.email,
-      stats: {total_games_played: user.games.count}
+      stats: {total_games_played: user.games.count},
+      subscription_status: status
     }
 
     [info, nil]
+  end
+
+  private
+
+  def retrieve_subscription_status(user)
+    status = @billing_api.get_subscription_status(user.id)
+    [status, nil]
+  rescue HttpError => e
+    [nil, [e.message]]
   end
 end
--- spec/use_cases/retrieve_user_info_spec.rb
+++ spec/use_cases/retrieve_user_info_spec.rb
@@ -3,15 +3,35 @@ require "rails_helper"
 RSpec.describe RetrieveUserInfo do
   it "returns current user info" do
     games = [double(Game), double(Game)]
-    user = double(User, id: 1, email: "email@example.com")
+    user = instance_double(User, id: 1, email: "email@example.com")
+    billing_api = instance_double(Billing::Api)
+    status = "active"
 
     allow(user).to receive(:games).and_return(games)
+    allow(billing_api).to receive(:get_subscription_status).with(user.id).and_return(status)
 
-    result, err = described_class.new.call(user)
+    result, err = described_class.new(billing_api:).call(user)
 
     expect(result[:id]).to eq user.id
     expect(result[:email]).to eq user.email
     expect(result[:stats][:total_games_played]).to eq games.count
+    expect(result[:subscription_status]).to eq status
     expect(err).to be_nil
+  end
+
+  it "returns error when subscription status could not be retrieved" do
+    games = [double(Game), double(Game)]
+    user = instance_double(User, id: 1, email: "email@example.com")
+    billing_api = instance_double(Billing::Api)
+    error = HttpError.new("not working...", 500)
+
+
+    allow(user).to receive(:games).and_return(games)
+    allow(billing_api).to receive(:get_subscription_status).with(user.id).and_raise(error)
+
+    result, err = described_class.new(billing_api:).call(user)
+
+    expect(result).to be_nil
+    expect(err).to eq([error.message])
   end
 end
